a155523b8a07b59eab03d49fdd23c619e330f63c
kazuhiko1979 <challengevolley1979@gmail.com>
2025-01-02 10:46:34 +0900

    slice

--- lesson.go
+++ lesson.go
@@ -5,18 +5,24 @@ import (
 )
 
 func main() {
-	var a [2]int
-	a[0] = 100
-	a[1] = 200
-	fmt.Println(a)
 
-	// list
-	// var b [2]int = [2]int{100, 200}
-	// b = append(b, 300)
-	// fmt.Println(b)
+	n := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(n)
+	fmt.Println(n[2:4])
+	fmt.Println(n[:2])
+	fmt.Println(n[2:])
+	fmt.Println(n[:])
 
-	// slice
-	var b []int = []int{100, 200}
-	b = append(b, 300)
-	fmt.Print(b)
+	n[2] = 100
+	fmt.Println(n)
+
+	var board = [][]int{
+		{0, 1, 2},
+		{3, 4, 5},
+		{6, 7, 8},
+	}
+	fmt.Println(board)
+
+	n = append(n, 100, 200, 300, 400)
+	fmt.Println(n)
 }
