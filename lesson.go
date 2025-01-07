c05644ee9030e52e90c834890dc3aa3e9bc82d34
kazuhiko1979 <challengevolley1979@gmail.com>
2025-01-07 09:48:53 +0900

    error

--- lesson.go
+++ lesson.go
@@ -4,33 +4,26 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
 	"os"
 )
 
-func LogginSettings(logFile string) {
-	logfile, _ := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	multiLogfile := io.MultiWriter(os.Stdout, logfile)
-	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
-	log.SetOutput(multiLogfile)
-
-}
-
 func main() {
-	LogginSettings("test.log")
-	_, err := os.Open("fafafa")
+	file, err := os.Open("lesson.go")
 	if err != nil {
-		log.Fatalln("Exit", err)
+		log.Fatalln("Error!")
 	}
+	defer file.Close()
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatalln("Error")
+	}
+	fmt.Println(count, string(data))
 
-	log.Println("logging!")
-	log.Printf("%T %v\n", "test", "test")
-
-	log.Fatalf("%T %v", "test", "test")
-
-	log.Fatalln("error!")
-
-	fmt.Println("ok")
+	if err = os.Chdir("test"); err != nil {
+		// if err != nil {
+		log.Fatalln("Error")
+	}
 
 }
