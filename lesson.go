b2bfa68eee854a6e74fd35ceb49e7e691df89471
kazuhiko1979 <challengevolley1979@gmail.com>
2025-01-08 09:38:39 +0900

    add practice

--- lesson.go
+++ lesson.go
@@ -2,28 +2,49 @@
 
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-)
+import "fmt"
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
 
 func main() {
-	file, err := os.Open("lesson.go")
-	if err != nil {
-		log.Fatalln("Error!")
-	}
-	defer file.Close()
-	data := make([]byte, 100)
-	count, err := file.Read(data)
-	if err != nil {
-		log.Fatalln("Error")
-	}
-	fmt.Println(count, string(data))
 
-	if err = os.Chdir("test"); err != nil {
-		// if err != nil {
-		log.Fatalln("Error")
+	l := []int{100, 300, 23, 11, 23, 2, 4, 6, 4}
+
+	// // 最小値
+	// min := l[0]
+	// for _, v := range l {
+	// 	if v < min {
+	// 		min = v
+	// 	}
+	// }
+
+	// fmt.Printf("最小値: %d\n", min)
+
+	minValue := l[0]
+	for _, v := range l {
+		minValue = min(minValue, v)
 	}
 
+	fmt.Printf("最小値 %d\n", minValue)
+
+	m := map[string]int{
+		"apple":  200,
+		"banana": 300,
+		"grape":  150,
+		"orange": 80,
+		"papaya": 500,
+		"kiwi":   90,
+	}
+	// 合計
+	total := 0
+	for _, price := range m {
+		total += price
+	}
+
+	fmt.Println("果物の価格の合計: %d円\n", total)
 }
--- tempCodeRunnerFile.go
+++ tempCodeRunnerFile.go
@@ -1,1 +1,2 @@
-	case "mac":
+
+		fmt.Println(s)
