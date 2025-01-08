f40b9ab133a4cd1439f395c22d260640c68a68d2
kazuhiko1979 <challengevolley1979@gmail.com>
2025-01-08 19:10:39 +0900

    type assertion

--- lesson.go
+++ lesson.go
@@ -1,76 +1,34 @@
 // Go:build main
-/*
-python
-
-class Vertex(object):
-	def __init__(self, x, y):
-		self.x = x
-		self.y = y
-
-	def area(self):
-		return self.x * self.y
-
-	def scale(self, i):
-		self.x = self.x * i
-		self.y = self.y * i
-
-
-class Vertex3D(Vertex):
-	def __init__(self, x, y, z):
-		super.__init__(x, y)
-		self.z = z
-
-	def area_3d(self):
-		return self.x * self.y * self.z
-
-	def scale_3d(self, i):
-		self.x = self.x * i
-		self.y = self.y * i
-		self.z = self.z * i
-
-
-v = Vertex(3, 4, 5)
-v.scale(10)
-print(v.area())
-print(v.area_3d())
-*/
-
 package main
 
 import "fmt"
 
-type Human interface {
-	Say() string
-}
+func do(i interface{}) {
+	// // ii := i.(int)
+	// // ii *= 2
+	// // fmt.Println(ii)
 
-type Person struct {
-	Name string
-}
+	// ss := i.(string)
+	// fmt.Println(ss + "!")
 
-type Dog struct {
-	Name string
-}
-
-func (p *Person) Say() string {
-	p.Name = "Mr." + p.Name
-	fmt.Println(p.Name)
-	return p.Name
-}
-
-func DriveCar(human Human) {
-	if human.Say() == "Mr.Mike" {
-		fmt.Println("Drive")
-	} else {
-		fmt.Println("Get out")
+	switch v := i.(type) {
+	case int:
+		fmt.Println(v * 2)
+	case string:
+		fmt.Println(v + "!")
+	default:
+		fmt.Printf("I don't know %T\n", v)
 	}
 }
 
 func main() {
-	var mike Human = &Person{"Mike"}
-	var x Human = &Person{"X"}
-	var dog Dog = Dog{"dog"}
-	DriveCar(mike)
-	DriveCar(x)
-	DriveCar(dog)
+
+	do(10)
+	do("Mike")
+	do(true)
+
+	var i int = 10
+	ii := float64(10)
+	fmt.Println(i, ii)
 
 }
