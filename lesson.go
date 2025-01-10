982b2f49f15abe14ab2fc3acd36800121574a1b4
kazuhiko1979 <challengevolley1979@gmail.com>
2025-01-10 15:49:53 +0900

    changel range close

--- lesson.go
+++ lesson.go
@@ -2,58 +2,32 @@ package main
 
 import "fmt"
 
-func goroutine1(s []int, c chan int) {
+func goroutine(s []int, c chan int) {
 	sum := 0
 	for _, v := range s {
 		sum += v
+		c <- sum
 	}
-	c <- sum
+	close(c)
 }
 
 func main() {
 	s := []int{1, 2, 3, 4, 5}
-	c := make(chan int) // 15, 15 queue
-	go goroutine1(s, c)
-	go goroutine1(s, c)
-	x := <-c
-	fmt.Println(x)
-	y := <-c
-	fmt.Println(y)
-
+	c := make(chan int, len(s))
+	go goroutine(s, c)
+	for i := range c {
+		fmt.Println(i)
+	}
 }
 
-// // Go:build main
-// package main
+// ch := make(chan int, 2)
+// ch <- 100
+// fmt.Println(len(ch))
+// ch <- 200
+// fmt.Println(len(ch))
 
-// import (
-// 	"fmt"
-// 	"sync"
-// )
+// close(ch)
 
-// func goroutine(s string, wg *sync.WaitGroup) {
-// 	for i := 0; i < 5; i++ {
-// 		// time.Sleep(100 * time.Millisecond)
-// 		fmt.Println(s)
-// 	}
-// 	wg.Done()
-// }
-
-// func normal(s string) {
-// 	for i := 0; i < 5; i++ {
-// 		// time.Sleep(100 * time.Millisecond)
-// 		fmt.Println(s)
-// 	}
-
-// }
-
-// func main() {
-// 	var wg sync.WaitGroup
-// 	wg.Add(1)
-// 	wg.Add(1)
-
-// 	go goroutine("world", &wg)
-// 	normal("hello")
-// 	// time.Sleep(2000 * time.Millisecond)
-// 	wg.Wait()
-
+// for c := range ch {
+// 	fmt.Println(c)
 // }
