41f4f7e69286439cc7f90bfe37d6d71d569069a0
kazuhiko1979 <challengevolley1979@gmail.com>
2025-03-12 13:02:37 +0900

    add heartbeat

--- main.go
+++ main.go
@@ -2,81 +2,146 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 )
 
-func DoWorkVer1(done <-chan interface{}, palseInterval time.Duration) (<-chan interface{}, <-chan time.Time) {
-	heartbeat := make(chan interface{})
-	results := make(chan time.Time)
+type startGoroutineFn func(done <-chan interface{}, pulseInterval time.Duration) (heartbeat <-chan interface{})
 
-	go func() {
-		// defer close(heartbeat)
-		// defer close(results)
+func DoWorkFn(done <-chan interface{}, intList ...int) (startGoroutineFn, <-chan interface{}) {
 
-		heartbeatpalse := time.Tick(palseInterval)
-		workGen := time.Tick(2 * palseInterval)
+	intStream := make(chan interface{})
 
-		sendHeartBeatPalse := func() {
-			select {
-			case heartbeat <- struct{}{}:
-			default:
-			}
-		}
+	doWork := func(done <-chan interface{}, pulseInterval time.Duration) <-chan interface{} {
+		heartbeat := make(chan interface{})
 
-		sendResult := func(result time.Time) {
-			for {
-				select {
-				case <-done:
-					return
-				case <-heartbeatpalse:
-					sendHeartBeatPalse()
-				case results <- result.Local():
-					return
+		go func() {
+			defer close(heartbeat)
+			defer close(intStream)
+
+			pulse := time.Tick(pulseInterval)
+
+		valueLoop:
+			for _, i := range intList {
+				if i < 0 {
+					log.Printf("Warning: negative value detected: %v. Skipping...\n", i)
+					// return
+					continue
+				}
+				for {
+					select {
+					case <-pulse:
+						select {
+						case heartbeat <- struct{}{}:
+						default:
+						}
+					case intStream <- i:
+						continue valueLoop
+					case <-done:
+						return
+					}
 				}
 			}
-		}
+		}()
+		return heartbeat
+	}
+	return doWork, intStream
+}
 
-		// for {
-		for i := 0; i < 2; i++ {
+func newSteward(timeout time.Duration, maxRestarts int, startGoroutineFn startGoroutineFn) startGoroutineFn {
+	return func(done <-chan interface{}, pulseInterval time.Duration) <-chan interface{} {
+		heartbeat := make(chan interface{})
+		go func() {
+			defer close(heartbeat)
+			var wardDone chan interface{}
+			var wardHeartbeat <-chan interface{}
+			startWard := func() {
+				wardDone = make(chan interface{})
+				wardHeartbeat = startGoroutineFn(or(wardDone, done), timeout/2)
+			}
+
+			startWard()
+			pulse := time.Tick(pulseInterval)
+
+			restartCount := 0
+
+		monitorLoop:
+			for {
+				if restartCount >= maxRestarts {
+					log.Println("steward: max restarts reached, stopping monitoring.")
+					close(wardDone)
+					return
+				}
+
+				timeoutSignal := time.After(timeout)
+				for {
+					select {
+					case <-pulse:
+						select {
+						case heartbeat <- struct{}{}:
+						default:
+						}
+					case <-wardHeartbeat:
+						continue monitorLoop
+					case <-timeoutSignal:
+						log.Println("steward: ward unhealthy; restarting")
+						close(wardDone)
+						restartCount++
+						startWard()
+						continue monitorLoop
+					case <-done:
+						return
+					}
+				}
+			}
+		}()
+		return heartbeat
+	}
+}
+
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+
+	orDone := make(chan interface{})
+	go func() {
+		defer close(orDone)
+		switch len(channels) {
+		case 2:
 			select {
-			case <-done:
-				return
-			case <-heartbeatpalse:
-				sendHeartBeatPalse()
-
-			case result := <-workGen:
-				sendResult(result)
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			case <-channels[2]:
+			case <-or(append(channels[3:], orDone)...):
 			}
 		}
 	}()
-
-	return heartbeat, results
+	return orDone
 }
 
 func main() {
 
 	done := make(chan interface{})
+	defer close(done)
 
-	const timeout = 2 * time.Second
-	heartbeat, results := DoWorkVer1(done, timeout/2)
+	doWork, intStream := DoWorkFn(done, 1, 2, -1, -2, -4, -5, 3, 4, 5)
 
-	for {
-		select {
-		case _, ok := <-heartbeat:
-			if !ok {
-				return
-			}
-			fmt.Println("receive heartbeat")
+	doWorkWithSteward := newSteward(1*time.Second, 3, doWork)
+	doWorkWithSteward(done, 1*time.Second)
 
-		case r, ok := <-results:
-			if !ok {
-				return
-			}
-			fmt.Printf("receive result %v\n", r)
-
-		case <-time.After(timeout):
-			fmt.Println("worker gorutine is not healthy!")
-			return
-		}
+	for i := range intStream {
+		fmt.Printf("Received: %v\n", i)
 	}
+
+	fmt.Println("Processing completed. Exiting...")
+
 }
