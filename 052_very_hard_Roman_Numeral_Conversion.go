d6ac47b28c8b9b408d3c09bba5485db178d83f07
kazuhiko1979 <challengevolley1979@gmail.com>
2025-03-14 11:02:26 +0900

    add Go

--- 052_very_hard_Roman_Numeral_Conversion.go
+++ 052_very_hard_Roman_Numeral_Conversion.go
@@ -1,3 +1,37 @@
+/*
+Roman Numeral Conversion
+Create a function that will take either a string containing a roman numeral, or an integer.
+
+Given a string, return the integer value of that roman numeral.
+Given an integer, return the equivalent roman numeral.
+Symbols to Values
+I ➞ 1
+
+V ➞ 5
+
+X ➞ 10
+
+L ➞ 50
+
+C ➞ 100
+
+D ➞ 500
+
+M ➞ 1000
+Examples
+roman_numerals("V") ➞ 5
+
+roman_numerals("IV") ➞ 4
+
+roman_numerals("CII") ➞ 102
+
+roman_numerals(45) ➞ "XLV"
+
+roman_numerals(1666) ➞ "MDCLXVI"
+Notes
+Numerical and Roman Numeral inputs will be positive numbers between 1 and 9999.
+*/
+
 package main
 
 import (
